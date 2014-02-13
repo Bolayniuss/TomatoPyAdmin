cd3ea6e2afda80c8d1dabc1e5ef6a25936f20bda
Bolay Michael <michael.bolay@gmail.com>
2014-02-13 22:04:00 +0100

    tests

--- TomatoPyAdmin/settings.py
+++ TomatoPyAdmin/settings.py
@@ -58,8 +58,15 @@ WSGI_APPLICATION = 'TomatoPyAdmin.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'replicator',
+        'USER': 'root',
+        'PASSWORD': None,
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
+        'OPTIONS': {
+          'autocommit': True,
+        },
     }
 }
 
